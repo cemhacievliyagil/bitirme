0796404a0a8a9c23c126d91fb9b5f8ddf301ae5f
cemhacievliyagil <cemhacievliyagil@gmail.com>
2022-03-08 00:27:03 +0300

    keye_gore_report_added

--- web.py
+++ web.py
@@ -7,6 +7,7 @@ from unittest import result
 from flask import Flask, render_template,request
 from flask_bootstrap import Bootstrap
 import mysql.connector
+from numpy import hamming
 from news_page import calculate_it
 from kisaltma import kisaltmalar
 from tarihal import tarihal
@@ -99,7 +100,6 @@ def onerilen_ara():
     d = (datetime.now() - timedelta(1)).strftime('%d-%m-%Y')
     dun = str(d)
 
-    print(numInt)
     cnx = mysql.connector.connect(
     user='root',
     password='',
@@ -109,13 +109,15 @@ def onerilen_ara():
 
     if (numInt.capitalize() in kategoriler):
         cursor.execute("SELECT * FROM news_keys WHERE kategori = %s ", (numInt,))
-        data = cursor.fetchall()
         data = sorted(cursor.fetchall(),key=lambda x: x[5],reverse=True)
+        print(data)
         data = list(map(list,data))
         for y in data:
           y[5] = tarihal(str(y[5]))
+          
         goster = 1
         try:
+          
          result = calculate_it(numInt.lower())
         except KeyError:
          result = [""]
@@ -129,12 +131,12 @@ def onerilen_ara():
      else:
       cursor.execute("SELECT * FROM news_keys WHERE key1 = %s OR key2 = %s OR key3 = %s OR key4 = %s OR key5 = %s OR key1 = %s OR key2 = %s OR key3 = %s OR key4 = %s OR key5 = %s",(numInt,numInt,numInt,numInt,numInt,numInt.title(),numInt.title(),numInt.title(),numInt.title(),numInt.title()))
       goster = 1
-      data = cursor.fetchall()
       data = sorted(cursor.fetchall(),key=lambda x: x[5],reverse=True)
+      print(data)
       data = list(map(list,data))
       for y in data:
           y[5] = tarihal(str(y[5]))
-      
+          
       try:
        result = calculate_it(numInt)
       except KeyError:
@@ -260,6 +262,40 @@ def rapor1():
     cursor.execute("""SELECT site as Haber_Kaynağı, COUNT(*) AS Haber_Miktarı FROM news_keys WHERE tarih >= DATE_ADD(CURDATE(), INTERVAL -30 DAY) GROUP BY site ORDER by Haber_Miktarı DESC;""")
     res7 = cursor.fetchall()
     return render_template('rapor1.html',res1 = res1,res2 = res2,res3 = res3,res4 = res4,res5 = res5,res6 = res6,res7=res7)
+
+@app.route('/rapor2', methods =["GET","POST"])
+def rapor2():
+    return render_template('rapor2.html')
+
+@app.route('/rapor3', methods =["GET","POST"])
+def rapor3():
+  kisaltma = kisaltmalar()
+  haftalik = 1
+  aylik=1
+  num = request.form['a']
+  numInt = num
+  for x in kisaltma:
+        if (numInt.upper() == x[0]):
+            numInt= x[1]
+  cnx = mysql.connector.connect(
+    user='root',
+    password='',
+    host='localhost',
+    database='mysql')
+  cursor = cnx.cursor()
+  cursor.execute("""SELECT site, COUNT(link) AS Haber_Sayısı FROM news_keys WHERE (key1 = %s OR key2 = %s OR key3 = %s OR key4 = %s OR key5 = %s OR key6 = %s OR key7 = %s OR key8 = %s) and tarih >= DATE_ADD(CURDATE(), INTERVAL -7 DAY) 	GROUP BY site ORDER by Haber_Sayısı DESC;""",(numInt,numInt,numInt,numInt,numInt,numInt,numInt,numInt))
+  res1=cursor.fetchall()
+  if(len(res1) == 0):
+    res1 = "Bulunamadı"
+    haftalik =0
+  cursor.execute("""SELECT site, COUNT(link) AS Haber_Sayısı FROM news_keys WHERE (key1 = %s OR key2 = %s OR key3 = %s OR key4 = %s OR key5 = %s OR key6 = %s OR key7 = %s OR key8 = %s) and tarih >= DATE_ADD(CURDATE(), INTERVAL -30 DAY) 	GROUP BY site ORDER by Haber_Sayısı DESC;""",(numInt,numInt,numInt,numInt,numInt,numInt,numInt,numInt))
+  res2 = cursor.fetchall()
+  if(len(res2) == 0):
+    res2 = "Bulunamadı"
+    aylik =0
+  numInt = numInt.capitalize()
+  return render_template('rapor3.html',res1 = res1, res2=res2,numInt = numInt,haftalik=haftalik, aylik=aylik)
+
 
 
 if __name__ == "__main__":
